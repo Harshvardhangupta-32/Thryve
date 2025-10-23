df8bf4d0495acea3a63f43770fc7eb2eb05ed9d4
Harshvardhan Gupta <harshgupta0871@gmail.com>
2025-10-23 14:15:02 +0530

    feat: add motivational quotes local notifications (basic) #ISSUE-XX

---
+++ lib/data/quotes.dart
@@ -0,0 +1,7 @@
+const List<String> sampleQuotes = [
+  "Small steps every day lead to big changes.",
+  "Focus on progress, not perfection.",
+  "You are capable of amazing things.",
+  "Consistency beats intensity.",
+  "Start where you are. Use what you have. Do what you can."
+];
